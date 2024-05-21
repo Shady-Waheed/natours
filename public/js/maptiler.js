4c583a5693044e3e51729c3a559b1d5c7550a0c9
Shady Waheed <shekohitler@gmail.com>
2024-05-22 01:29:42 +0300

    fix the bug in maptiler file and complete the tour page

--- public/js/maptiler.js
+++ public/js/maptiler.js
@@ -4,7 +4,45 @@ console.log(locations);
 maptilersdk.config.apiKey = 'YExDCio1SPzRYHkzCi3Z';
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element to render the map
-  style: 'streets-v2',
-  center: [16.62662018, 49.2125578], // starting position [lng, lat]
-  zoom: 14, // starting zoom
+  style: 'streets-v2-light',
+  scrollZoom: false,
+  // center: [-118.113491,34.111745], // starting position [lng, lat]
+  zoom: 1, // starting zoom
+  // interactive: false,
+});
+
+const bounds = new maptilersdk.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new maptilersdk.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new maptilersdk.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend the map bounds to include the current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
